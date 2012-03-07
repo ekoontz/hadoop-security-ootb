94a05e513f9b0f214d4c24dd849face637932b57
Eugene Koontz <ekoontz@hiro-tan.org>
2012-03-07 18:32:41 +0000

    split daemon-starting out into two scripts

--- bin/runall-secure.sh
+++ bin/runall-secure.sh
@@ -4,38 +4,11 @@ bin=`which $0`
 bin=`dirname ${bin}`
 bin=`cd "$bin"; pwd`
 
-export HADOOP_SECURE_DN_USER=hdfs
+$bin/start-hdfs.sh
+$bin/start-yarn.sh
 
 #get sudo permission from user, if necessary: 'echo' is just an inoccuous thing to run to get the necessary permission.
 sudo echo 
-
-#kill any currently-running daemons, if any.
-sudo pkill -f NameNode
-sudo pkill -f DataNode
-sudo pkill -f ResourceManager
-sudo pkill -f NodeManager
-
-#wipe out existing install, if any.
-sudo rm -rf /tmp/hadoop/*
-sudo su hdfs -c "JAVA_HOME=$JAVA_HOME $bin/hdfs namenode -format"
-
-sudo su hdfs -c "JAVA_HOME=$JAVA_HOME $bin/hdfs namenode &"
-
-#sleep because it seems like possibly dn will give up and exit
-#if namenode doesn't start fast enough.
-sleep 5
-sudo su root -c "JAVA_HOME=$JAVA_HOME HADOOP_SECURE_DN_USER=hdfs $bin/hdfs datanode &"
-
-sudo su yarn -c "JAVA_HOME=$JAVA_HOME $bin/yarn resourcemanager &"
-
-#sleep because nodemanager will die pretty quickly without
-#a resourcemanager (MAPREDUCE-3676)
-sleep 5
-
-sudo mkdir -p /tmp/hadoop/nm-local-dirs
-sudo chown -R yarn /tmp/hadoop/nm-local-dirs
-
-sudo su yarn -c "JAVA_HOME=$JAVA_HOME $bin/yarn nodemanager &"
 
 #finally, set permissions.
 #first, set up credentials so that we have nn's permission to modify HDFS.
---
+++ bin/start-hdfs.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+# no secondary namenode yet.
+set -x
+
+bin=`which $0`
+bin=`dirname ${bin}`
+bin=`cd "$bin"; pwd`
+
+# <look in source directory's classes first to avoid need to recompile and package everything. -->
+export HADOOP_USER_CLASSPATH_FIRST=true
+export HADOOP_CLASSPATH=/home/ec2-user/hadoop-common/hadoop-hdfs-project/hadoop-hdfs/target/classes
+
+export HADOOP_SECURE_DN_USER=hdfs
+
+#get sudo permission from user, if necessary: 'echo' is just an inoccuous thing to run to get the necessary permission.
+sudo echo 
+
+#kill any currently-running daemons, if any.
+sudo pkill -f NameNode
+sudo pkill -f DataNode
+
+#wipe out existing install, if any.
+sudo rm -rf /tmp/hadoop/*
+sudo su hdfs -c "JAVA_HOME=$JAVA_HOME $bin/hdfs namenode -format"
+
+sudo su hdfs -c "HADOOP_CLASSPATH=$HADOOP_CLASSPATH HADOOP_USER_CLASSPATH_FIRST=true JAVA_HOME=$JAVA_HOME $bin/hdfs namenode &"
+
+#sleep because it seems like possibly dn will give up and exit
+#if namenode doesn't start fast enough.
+sleep 5
+sudo su root -c "JAVA_HOME=$JAVA_HOME HADOOP_SECURE_DN_USER=hdfs $bin/hdfs datanode &"
