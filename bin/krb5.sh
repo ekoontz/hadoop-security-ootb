575cbf0825986c46efc39bc72be12ec46fd41e49
Eugene Koontz <ekoontz@hiro-tan.org>
2012-03-08 20:12:13 +0000

    stop and start krb5kdc as appropriate

--- bin/krb5.sh
+++ bin/krb5.sh
@@ -6,9 +6,14 @@ if [ $EUID != 0 ]; then
     exit
 fi
 
+/etc/init.d/krb5kdc stop
+
 yum -y install krb5-server
+
 kdb5_util create -r HADOOP.LOCALDOMAIN -s
+
 /etc/init.d/krb5kdc start
+
 
 echo "modprinc -maxrenewlife 1week K/M@HADOOP.LOCALDOMAIN" | kadmin.local
 echo "modprinc -maxrenewlife 1week krbtgt/HADOOP.LOCALDOMAIN@HADOOP.LOCALDOMAIN" | kadmin.local
