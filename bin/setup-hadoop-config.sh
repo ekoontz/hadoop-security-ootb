2480655e79182b46c6a63c5126f8c75c9fc0ac3f
Eugene Koontz <ekoontz@hiro-tan.org>
2012-03-10 15:57:51 +0000

    add addusers script; run setup-hadoop-config correctly

--- bin/setup-hadoop-config.sh
+++ bin/setup-hadoop-config.sh
@@ -2,10 +2,14 @@
 bin=`which $0`
 bin=`dirname ${bin}`
 bin=`cd "$bin"; pwd`
+if [ -z $MASTER ]; then
+    echo "you must define \$MASTER in your environment."
+    exit 1
+fi
 
 LOCAL_ETC=$bin/../etc/hadoop
 ETC=/usr/lib/hadoop/etc/hadoop
-cat $LOCAL_ETC/core-site.template.xml | sed s/\\\${MASTER}/`hostname -f`/g > $ETC/core-site.xml
+cat $LOCAL_ETC/core-site.template.xml | sed s/\\\${MASTER}/$MASTER/g > $ETC/core-site.xml
 
 mv $ETC/hdfs-site.xml /tmp
 ln -s $ETC/core-site.xml $ETC/hdfs-site.xml
--- bin/setup-slave.sh
+++ bin/setup-slave.sh
@@ -8,23 +8,32 @@ if [ ! -f $SSH_PRIVATE_KEY ]; then
     echo "you must have the master's private key in $SSH_PRIVATE_KEY so that the keytab may be fetched."
     exit
 fi
-
                                                               
 bin=`which $0`
 bin=`dirname ${bin}`
 bin=`cd "$bin"; pwd`
 
+sudo $bin/addusers.sh
+
 cat $bin/../etc/hadoop/krb5.conf | sed s/\\\${MASTER_HOST}/$MASTER/  >  $bin/../etc/krb5.conf
 sudo cp $bin/../etc/krb5.conf /etc
 
-sudo $bin/setup-hadoop-config.sh
+sudo su - -c "MASTER=$MASTER /home/ec2-user/hadoop-security-ootb/bin/setup-hadoop-config.sh"
+
+RESULT=$?
+
+if [ ! -z $RESULT ]; then
+    exit $RESULT
+fi
 
 sudo mkdir -p /usr/lib/hadoop/etc/hadoop/security
 sudo scp -i $SSH_PRIVATE_KEY ec2-user@$MASTER:hadoop-security-ootb/etc/hdfs.slave.keytab /usr/lib/hadoop/etc/hadoop/security/hdfs.keytab
+sudo chown hdfs /usr/lib/hadoop/etc/hadoop/security/hdfs.keytab
+
 sudo scp -i $SSH_PRIVATE_KEY ec2-user@$MASTER:hadoop-security-ootb/etc/yarn.slave.keytab /usr/lib/hadoop/etc/hadoop/security/yarn.keytab
 sudo chown yarn /usr/lib/hadoop/etc/hadoop/security/yarn.keytab
 
 #test that keytabs work.
 set -x
-kinit -k -t /usr/lib/hadoop/etc/hadoop/security/hdfs.keytab hdfs/`hostname -f`
-kinit -k -t /usr/lib/hadoop/etc/hadoop/security/yarn.keytab yarn/`hostname -f`
+sudo su hdfs -c "kinit -k -t /usr/lib/hadoop/etc/hadoop/security/hdfs.keytab hdfs/`hostname -f`"
+sudo su yarn -c "kinit -k -t /usr/lib/hadoop/etc/hadoop/security/yarn.keytab yarn/`hostname -f`"
