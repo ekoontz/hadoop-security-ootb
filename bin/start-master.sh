e4aeacf87479c4aefbb48058e60c7e40e167060e
Eugene Koontz <ekoontz@hiro-tan.org>
2012-03-09 18:18:43 -0800

    start-master calls set-master

--- bin/start-master.sh
+++ bin/start-master.sh
@@ -4,10 +4,12 @@ bin=`dirname ${bin}`
 bin=`cd "$bin"; pwd`
 
 $bin/shutdown.sh
+$bin/setup-master.sh
 
 $bin/start-hdfs-master.sh
 $bin/start-yarn-master.sh
 
+#need to wait for one datanode to come up before we can do this:
 #export JAVA_HOME=/usr/lib/jvm/jre-openjdk
 #sudo su hdfs -c "JAVA_HOME=$JAVA_HOME $bin/hdfs-permissions.sh"
 
